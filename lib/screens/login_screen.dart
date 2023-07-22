216fab470f0c4af477cb5bf62729461963a3980c
McJaxon <Mcjaxon12@gmail.com>
2023-07-22 12:55:26 +0300

    latest code

--- lib/screens/login_screen.dart
+++ lib/screens/login_screen.dart
@@ -1,6 +1,9 @@
 import 'package:flutter/material.dart';
 import 'package:lottie/lottie.dart';
+import 'package:taasa_life/authentications/firebase_auth.dart';
+import 'package:taasa_life/decortaions/loading.dart';
 import 'package:taasa_life/decortaions/styles.dart';
+import 'package:taasa_life/screens/main_screen.dart';
 
 class LogInScreen extends StatefulWidget {
   final VoidCallback switchUser;
@@ -20,6 +23,10 @@ class _LogInScreenState extends State<LogInScreen>
         AnimationController(vsync: this, duration: const Duration(seconds: 3));
   }
 
+  final _formKey = GlobalKey<FormState>();
+  final emailController = TextEditingController();
+  final passwordController = TextEditingController();
+
   @override
   void dispose() {
     super.dispose();
@@ -27,105 +34,143 @@ class _LogInScreenState extends State<LogInScreen>
   }
 
   bool bookMarked = false;
+  bool loading = false;
   @override
   Widget build(BuildContext context) {
-    return Scaffold(
-      body: Container(
-          height: double.infinity,
-          width: double.infinity,
-          padding: const EdgeInsets.all(10),
-          decoration: Decorations.bgDecoration,
-          child: ListView(
-            children: [
-              const SizedBox(
-                height: 250,
-              ),
-              const Align(
-                alignment: Alignment.centerLeft,
-                child: Column(
-                  crossAxisAlignment: CrossAxisAlignment.start,
-                  children: [
-                    SizedBox(
-                        child: Text(
-                      'Taasa Life',
-                      style:
-                          TextStyle(fontSize: 30, fontWeight: FontWeight.bold),
-                    )),
-                    SizedBox(
-                        child: Text(
-                      'Please Login',
-                      style:
-                          TextStyle(fontSize: 30, fontWeight: FontWeight.bold),
-                    )),
-                  ],
-                ),
-              ),
-              SizedBox(
-                height: 100,
-                width: 70,
-                child: GestureDetector(
-                  child: Lottie.network(
-                      'https://assets3.lottiefiles.com/packages/lf20_T9jCikauOc.json',
-                      controller: _controller),
-                ),
-              ),
-              const SizedBox(
-                height: 10,
-              ),
-              Form(
-                  child: Column(
-                children: [
-                  SizedBox(
-                      child: TextFormField(
-                    decoration: Decorations.decoration
-                        .copyWith(label: const Text('Email/phone')),
-                  )),
-                  const SizedBox(
-                    height: 10,
-                  ),
-                  SizedBox(
-                      child: TextFormField(
-                    decoration: Decorations.decoration
-                        .copyWith(label: const Text('Password')),
-                  )),
-                  const SizedBox(
-                    height: 10,
-                  ),
-                  GestureDetector(
-                    child: ElevatedButton(
-                        onPressed: () {
-                          if (bookMarked == false) {
-                            bookMarked = true;
-                            _controller.forward();
-                          } else {
-                            bookMarked = false;
-                            _controller.reverse();
-                          }
-                        },
-                        child: const Text('LogIn')),
-                  ),
-                ],
-              )),
-              const SizedBox(
-                height: 120,
-              ),
-              Container(
-                padding: const EdgeInsets.only(left: 90),
-                child: Row(
+    final FirebaseAuthentication accessFirebase = FirebaseAuthentication();
+    return loading
+        ? const Loading()
+        : Scaffold(
+            body: Container(
+                height: double.infinity,
+                width: double.infinity,
+                padding: const EdgeInsets.all(10),
+                decoration: Decorations.bgDecoration,
+                child: ListView(
                   children: [
                     const SizedBox(
-                      child: Text(' Should Create Account'),
+                      height: 250,
+                    ),
+                    const Align(
+                      alignment: Alignment.centerLeft,
+                      child: Column(
+                        crossAxisAlignment: CrossAxisAlignment.start,
+                        children: [
+                          SizedBox(
+                              child: Text(
+                            'Taasa Life',
+                            style: TextStyle(
+                                fontSize: 30, fontWeight: FontWeight.bold),
+                          )),
+                          SizedBox(
+                              child: Text(
+                            'Please Login',
+                            style: TextStyle(
+                                fontSize: 30, fontWeight: FontWeight.bold),
+                          )),
+                        ],
+                      ),
                     ),
                     SizedBox(
-                      child: TextButton(
-                          onPressed: widget.switchUser,
-                          child: const Text('SignUp')),
+                      height: 100,
+                      width: 70,
+                      child: GestureDetector(
+                        child: Lottie.network(
+                            'https://assets3.lottiefiles.com/packages/lf20_T9jCikauOc.json',
+                            controller: _controller),
+                      ),
+                    ),
+                    const SizedBox(
+                      height: 10,
+                    ),
+                    Form(
+                        key: _formKey,
+                        child: Column(
+                          children: [
+                            SizedBox(
+                                child: TextFormField(
+                              controller: emailController,
+                              decoration: Decorations.decoration
+                                  .copyWith(label: const Text('Email/phone')),
+                            )),
+                            const SizedBox(
+                              height: 10,
+                            ),
+                            SizedBox(
+                                child: TextFormField(
+                              obscureText: true,
+                              controller: passwordController,
+                              decoration: Decorations.decoration
+                                  .copyWith(label: const Text('Password')),
+                            )),
+                            const SizedBox(
+                              height: 10,
+                            ),
+                            GestureDetector(
+                              child: ElevatedButton(
+                                  onPressed: () {
+                                    if (_formKey.currentState!.validate()) {
+                                      setState(() {
+                                        loading = true;
+                                      });
+                                      if (bookMarked == false) {
+                                        bookMarked = true;
+                                        _controller.forward();
+                                        accessFirebase
+                                            .signInEmail(emailController.text,
+                                                passwordController.text)
+                                            .then((value) => Navigator.push(
+                                                context,
+                                                PageRouteBuilder(
+                                                    transitionDuration:
+                                                        const Duration(
+                                                            milliseconds: 500),
+                                                    transitionsBuilder:
+                                                        (context,
+                                                            animation,
+                                                            animationTime,
+                                                            child) {
+                                                      return ScaleTransition(
+                                                          scale: animation,
+                                                          alignment:
+                                                              Alignment.center,
+                                                          child: child);
+                                                    },
+                                                    pageBuilder: (context,
+                                                        animation,
+                                                        animationTime) {
+                                                      return const MainScreen();
+                                                    })));
+                                      } else {
+                                        bookMarked = false;
+                                        _controller.reverse();
+                                      }
+                                    }
+                                  },
+                                  child: const Text('LogIn')),
+                            ),
+                          ],
+                        )),
+                    const SizedBox(
+                      height: 120,
+                    ),
+                    Container(
+                      padding: const EdgeInsets.only(left: 90),
+                      child: Row(
+                        children: [
+                          const SizedBox(
+                            child: Text(' Should Create Account'),
+                          ),
+                          SizedBox(
+                            child: TextButton(
+                                onPressed: widget.switchUser,
+                                child: const Text('SignUp')),
+                          )
+                        ],
+                      ),
                     )
                   ],
-                ),
-              )
-            ],
-          )),
-    );
+                )),
+          );
   }
 }
--- lib/screens/signup_screen.dart
+++ lib/screens/signup_screen.dart
@@ -1,5 +1,7 @@
 import 'package:flutter/material.dart';
 import 'package:lottie/lottie.dart';
+import 'package:taasa_life/authentications/firebase_auth.dart';
+import 'package:taasa_life/decortaions/loading.dart';
 import 'package:taasa_life/decortaions/styles.dart';
 
 import 'profile_screen.dart';
@@ -22,144 +24,175 @@ class _SignUpScreenState extends State<SignUpScreen>
         AnimationController(vsync: this, duration: const Duration(seconds: 2));
   }
 
+  final _formKey = GlobalKey<FormState>();
+  final emailController = TextEditingController();
+  final passwordController = TextEditingController();
+  final confirmPasswordController = TextEditingController();
+
   @override
   void dispose() {
     super.dispose();
     _controller.dispose();
   }
 
+  bool _loading = false;
   bool bookMarked = false;
+
   @override
   Widget build(BuildContext context) {
-    return Scaffold(
-      body: Container(
-          height: double.infinity,
-          width: double.infinity,
-          padding: const EdgeInsets.all(10),
-          decoration: Decorations.bgDecoration,
-          child: ListView(
-            children: [
-              const SizedBox(
-                height: 210,
-              ),
-              const Align(
-                alignment: Alignment.centerLeft,
-                child: Column(
-                  crossAxisAlignment: CrossAxisAlignment.start,
+    final FirebaseAuthentication accessFirebase = FirebaseAuthentication();
+    return _loading
+        ? const Loading()
+        : Scaffold(
+            body: Container(
+                height: double.infinity,
+                width: double.infinity,
+                padding: const EdgeInsets.all(10),
+                decoration: Decorations.bgDecoration,
+                child: ListView(
                   children: [
-                    SizedBox(
-                        child: Text(
-                      'Welcome ',
-                      style:
-                          TextStyle(fontSize: 25, fontWeight: FontWeight.bold),
-                    )),
-                    SizedBox(
-                        child: Text(
-                      'Taasa Life',
-                      style:
-                          TextStyle(fontSize: 30, fontWeight: FontWeight.bold),
-                    )),
-                  ],
-                ),
-              ),
-              SizedBox(
-                height: 100,
-                width: 70,
-                child: GestureDetector(
-                  // onTap: () {
-                  //   if (bookMarked == false) {
-                  //     bookMarked = true;
-                  //     _controller.forward();
-                  //   } else {
-                  //     bookMarked = false;
-                  //     _controller.reverse();
-                  //   }
-                  // },
-                  child: Lottie.network(
-                      'https://assets3.lottiefiles.com/packages/lf20_T9jCikauOc.json',
-                      controller: _controller),
-                ),
-              ),
-              const SizedBox(
-                height: 10,
-              ),
-              Form(
-                child: Column(
-                  children: [
-                    SizedBox(
-                        child: TextFormField(
-                      decoration: Decorations.decoration
-                          .copyWith(label: const Text('Email/phone')),
-                    )),
                     const SizedBox(
-                      height: 10,
+                      height: 210,
+                    ),
+                    const Align(
+                      alignment: Alignment.centerLeft,
+                      child: Column(
+                        crossAxisAlignment: CrossAxisAlignment.start,
+                        children: [
+                          SizedBox(
+                              child: Text(
+                            'Welcome ',
+                            style: TextStyle(
+                                fontSize: 25, fontWeight: FontWeight.bold),
+                          )),
+                          SizedBox(
+                              child: Text(
+                            'Taasa Life',
+                            style: TextStyle(
+                                fontSize: 30, fontWeight: FontWeight.bold),
+                          )),
+                        ],
+                      ),
                     ),
                     SizedBox(
-                        child: TextFormField(
-                      decoration: Decorations.decoration
-                          .copyWith(label: const Text('Create Password')),
-                    )),
-                    const SizedBox(
-                      height: 10,
-                    ),
-                    SizedBox(
-                        child: TextFormField(
-                      decoration: Decorations.decoration
-                          .copyWith(label: const Text('Confirm Password')),
-                    )),
-                    const SizedBox(
-                      height: 10,
-                    ),
-                    ElevatedButton(
-                        onPressed: () {
-                          if (bookMarked == false) {
-                            bookMarked = true;
-                            _controller.forward().then((value) =>
-                                Navigator.push(
-                                    context,
-                                    PageRouteBuilder(
-                                        transitionDuration:
-                                            const Duration(milliseconds: 500),
-                                        transitionsBuilder: (context, animation,
-                                            animationTime, child) {
-                                          return ScaleTransition(
-                                              scale: animation,
-                                              alignment: Alignment.center,
-                                              child: child);
-                                        },
-                                        pageBuilder: (context, animation,
-                                            animationTime) {
-                                          return const ProfileScreen();
-                                        })));
-                          } else {
-                            bookMarked = false;
-                            _controller.reverse();
-                          }
-                        },
-                        child: const Text('Sign Up')),
-                    const SizedBox(
                       height: 100,
+                      width: 70,
+                      child: GestureDetector(
+                        // onTap: () {
+                        //   if (bookMarked == false) {
+                        //     bookMarked = true;
+                        //     _controller.forward();
+                        //   } else {
+                        //     bookMarked = false;
+                        //     _controller.reverse();
+                        //   }
+                        // },
+                        child: Lottie.network(
+                            'https://assets3.lottiefiles.com/packages/lf20_T9jCikauOc.json',
+                            controller: _controller),
+                      ),
                     ),
-                  ],
-                ),
-              ),
-              Container(
-                padding: const EdgeInsets.only(left: 90),
-                child: Row(
-                  children: [
                     const SizedBox(
-                      child: Text('Already Have Account'),
+                      height: 10,
                     ),
-                    SizedBox(
-                      child: TextButton(
-                          onPressed: widget.switchUser,
-                          child: const Text('LogIn')),
+                    Form(
+                      key: _formKey,
+                      child: Column(
+                        children: [
+                          SizedBox(
+                              child: TextFormField(
+                            controller: emailController,
+                            decoration: Decorations.decoration
+                                .copyWith(label: const Text('Email/phone')),
+                            validator: (value) =>
+                                value!.isEmpty ? 'Please provide Email' : null,
+                          )),
+                          const SizedBox(
+                            height: 10,
+                          ),
+                          SizedBox(
+                              child: TextFormField(
+                            controller: passwordController,
+                            decoration: Decorations.decoration
+                                .copyWith(label: const Text('Create Password')),
+                            validator: (value) =>
+                                value!.length <= 7 ? 'Invalid Password' : null,
+                          )),
+                          const SizedBox(
+                            height: 10,
+                          ),
+                          SizedBox(
+                              child: TextFormField(
+                            controller: confirmPasswordController,
+                            decoration: Decorations.decoration.copyWith(
+                                label: const Text('Confirm Password')),
+                          )),
+                          const SizedBox(
+                            height: 10,
+                          ),
+                          ElevatedButton(
+                              onPressed: () {
+                                if (_formKey.currentState!.validate()) {
+                                  setState(() {
+                                    _loading = true;
+                                  });
+                                  if (bookMarked == false) {
+                                    bookMarked = true;
+                                    _controller.forward();
+                                    accessFirebase
+                                        .createEmailAccount(
+                                            emailController.text,
+                                            passwordController.text)
+                                        .then((value) => Navigator.push(
+                                            context,
+                                            PageRouteBuilder(
+                                                transitionDuration:
+                                                    const Duration(
+                                                        milliseconds: 500),
+                                                transitionsBuilder: (context,
+                                                    animation,
+                                                    animationTime,
+                                                    child) {
+                                                  return ScaleTransition(
+                                                      scale: animation,
+                                                      alignment:
+                                                          Alignment.center,
+                                                      child: child);
+                                                },
+                                                pageBuilder: (context,
+                                                    animation, animationTime) {
+                                                  return const ProfileScreen();
+                                                })));
+                                  } else {
+                                    bookMarked = false;
+                                    _controller.reverse();
+                                  }
+                                }
+                              },
+                              child: const Text('Sign Up')),
+                          const SizedBox(
+                            height: 100,
+                          ),
+                        ],
+                      ),
+                    ),
+                    Container(
+                      padding: const EdgeInsets.only(left: 90),
+                      child: Row(
+                        children: [
+                          const SizedBox(
+                            child: Text('Already Have Account'),
+                          ),
+                          SizedBox(
+                            child: TextButton(
+                                onPressed: widget.switchUser,
+                                child: const Text('LogIn')),
+                          )
+                        ],
+                      ),
                     )
                   ],
-                ),
-              )
-            ],
-          )),
-    );
+                )),
+          );
   }
 }
